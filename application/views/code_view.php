da4bad1eb52ac4a7d697134a02704cda9c98ef03
Amandeep Singh <amandeepsingh0929@gmail.com>
2016-02-05 18:08:48 -0500

    Alert Box added

--- application/views/code_view.php
+++ application/views/code_view.php
@@ -3,25 +3,38 @@
 <head>
 	<meta charset="utf-8">
 	<title>Code practice for TBS</title>
+	 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	 
 	<script src="<?php echo base_url('assets/jquery.js'); ?>" type="text/javascript"></script>
+	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 	<script> 
 	var base_url = '<?php echo base_url(); ?>';
 	</script>
+
 </head>
 <body>
 
 <div id="container">
 	<div id="body">
-		<form action="#">
+		<form onsubmit = "return false">
 		  Enter Text:<br>
 		  <input id="text_value"  type="text" name="enter_text" value=""><br><br>
 		  <input id="create_log" type="button" value="Create Log">
 		</form>
 	</div>
 </div>
-
+<!-- ALERT BOX STARTS -->
+<div id="dialog" title="Alert !">
+  <p id="alert_text"></p>
+</div>
+<!-- ALERT BOX ENDS -->
 </body>
 <script>
+$(function() {
+    $( "#dialog" ).dialog({
+	  autoOpen: false
+	});
+});
 /**
  * Function fires on click of Create Log Button.
  *
@@ -32,7 +45,8 @@ $( "#create_log" ).click(function() {
 	var text_value = $('#text_value').val(); // value fetched form text field
 	
 	if(text_value == ''){
-		alert('Please Enter text');
+		$( "#alert_text" ).text("Please Enter text");
+		$( "#dialog" ).dialog("open");
 	}else{
 		$.ajax({
 			url: base_url + 'code/createLog',
@@ -41,7 +55,8 @@ $( "#create_log" ).click(function() {
 				text_value:text_value
 			},
 			success:function(data){
-				alert(data);
+				$( "#alert_text" ).text(data);
+				$( "#dialog" ).dialog("open");
 			}
 		}) 
 	}
